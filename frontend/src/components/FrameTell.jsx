587941b1e4a73ca6cdcdb22cde274dcf56a6d1d8
Shreeyanch Shrestha <shsh19cs@cmrit.ac.in>
2025-01-29 18:16:26 +0545

    first commitssfsxvdssd

--- frontend/src/components/FrameTell.jsx
+++ frontend/src/components/FrameTell.jsx
@@ -3,16 +3,17 @@ import PropTypes from "prop-types";
 const FrameTell = ({ className = "", onClose }) => {
   return (
     <div
-      className={`w-[848px] relative flex flex-col items-center justify-start max-w-full max-h-full overflow-auto ${className}`}
+      className={`w-full relative flex flex-row items-center justify-start gap-2.5 max-w-full max-h-full overflow-auto ${className}`}
     >
       <img
-        className="w-full h-auto object-cover"
-        alt="Background"
-        src="/rectangle-2@2x.png"
+        className="w-full h-auto object-fill z-[0]" // Ensure it covers the div properly
+        alt=""
+        src="/ppp.png"
       />
+      {/* Close button positioned at the top-right corner */}
       <img
-        className="w-[21px] h-[21px] absolute top-[12px] right-[12px] cursor-pointer"
-        alt="Close"
+        className="w-[21px] absolute top-2 right-2 h-[21px] cursor-pointer z-[1]"
+        alt=""
         src="/frame-1410103927.svg"
         onClick={onClose}
       />
